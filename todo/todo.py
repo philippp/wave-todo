c34bd7d2adbead650d3392af88c907f2f6aa9cf5
Philippp <philippp@philipp-pfeiffenbergers-macbook-pro.local>
2009-05-31 03:26:31 -0700

    Todo bot saves wave as todoitem on addition and has a hook to receive instructions

--- todo/todo.py
+++ todo/todo.py
@@ -10,6 +10,9 @@ from waveapi import robot
 
 from models import TodoItem
 
+DEBUG = True
+BOT_NAME = 'Taskmop'
+CMD_LIST_TASKS = 'show'
 
 def OnParticipantsChanged(properties, context):
   """Invoked when any participants have been added/removed."""
@@ -20,9 +23,25 @@ def OnParticipantsChanged(properties, context):
       break
 
 def OnBlipSubmitted(properties, context):
-  logging.debug(dir(properties))
-  logging.debug(str(properties))
-  logging.debug(str(properties.items()))
+  """ Called when user submits a blip, checks if it is a command """
+  root_wavelet = context.GetRootWavelet()
+  newBlip = context.GetBlipById( properties['blipId'] )
+  textAdded = str(newBlip.GetDocument().GetText())
+
+  if "%s: %s" % BOT_NAME, CMD_LIST_TASKS in textAdded:
+    todoItems = TodoItem.TodoItem.all()
+    newBlip.CreateChild().GetDocument().SetText("There are todo items "+str(len( todoItems )) )
+  elif "%s: help" % BOT_NAME in textAdded:
+    newBlip.CreateChild().GetDocument().SetText(Help())    
+  elif DEBUG:
+    newBlip.CreateChild().GetDocument().SetText("I do not understand %s" % textAdded)    
+
+def Help():
+  """ Generate and return a help string """
+  help = ""
+  for cmd, desc in {CMD_LIST_TASKS:"List all Tasks"}.items()
+    help += "%s: %s - %s" % (BOT_NAME, cmd, desc)
+  return help
 
 def Setup(context):
   """Called when this robot is first added to the wave."""
@@ -37,11 +56,11 @@ def Setup(context):
 
 
 if __name__ == '__main__':
-  todo = robot.Robot('Taskmop',
+  todo = robot.Robot(BOT_NAME,
                       image_url='http://dummy.appspot.com/icon.png',
                       profile_url='http://philippp.appspot.com/')
-  dummy.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED,
-                        OnParticipantsChanged)
-  dummy.RegisterHandler(events.WAVELET_BLIP_CREATED,
+  todo.RegisterHandler(events.BLIP_SUBMITTED,
                         OnBlipSubmitted)
-  dummy.Run()
+  todo.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED,
+                        OnParticipantsChanged)
+  todo.Run()
